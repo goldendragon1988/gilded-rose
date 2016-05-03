c95dab59a13c3d1deb0217a34e9a061d9b56d466
Jim Arnold Go <jag.arnold.go@gmail.com>
2016-05-03 19:02:44 +0800

    Finished

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,52 +6,68 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      check_category(item)
     end
   end
+
+  def check_category(item)
+    name = item_name(item)
+    if name.include?('sulfuras')
+      # Do nothing
+    elsif name.include?('aged brie')
+      the_reverse(item)
+    elsif name.include?('backstage passes')
+      the_concert(item)
+    elsif name.include?('conjured')
+      the_flash(item)
+    else
+      the_ordinary(item)
+    end
+  end
+
+  def the_reverse(item)
+    minus_sell_in(item)
+    item.sell_in > 0 ? (plus_quality(item)) : (2.times{plus_quality(item)})
+  end
+
+  def the_flash(item)
+    minus_sell_in(item)
+    item.sell_in > 0 ? 2.times{minus_quality(item)} : 4.times{minus_quality(item)}
+  end
+
+  def the_concert(item)
+    minus_sell_in(item)
+    if item.sell_in <= 0
+      item.quality = 0
+    elsif item.sell_in <= 5
+      3.times{plus_quality(item)}
+    else item.sell_in <= 10
+      2.times{plus_quality(item)}
+    end
+  end
+
+  def the_ordinary(item)
+    minus_sell_in(item)
+    item.sell_in > 0 ? (minus_quality(item)) : (2.times{minus_quality(item)})
+  end
+
+  def plus_quality(item)
+    item.quality += 1 if item.quality < 50
+  end
+
+  def minus_quality(item)
+    item.quality -= 1 if item.quality > 0
+  end
+
+  def minus_sell_in(item)
+    item.sell_in -= 1 if item.sell_in > 0
+  end
+
+  def item_name(item)
+    item.name.downcase
+  end
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
@@ -66,3 +82,15 @@ class Item
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
+
+items = [
+   Item.new('Backstage passes to a secret court', 4, 10),
+   Item.new('Backstage passes to a concert', 5, 6),
+   Item.new('Aged Brie', 10, 50),
+   Item.new('Sulfuras', 33, 80),
+   Item.new('conJured', 33, 10),
+   Item.new('chte ocmms', 33, 40)
+]
+
+z = GildedRose.new(items)
+z.update_quality
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,20 +1,118 @@
 require 'spec_helper'
- 
+
 describe GildedRose do
+  let!(:sell_in) { 5 }
+  let!(:quality) { 5 }
+  let!(:item) { Item.new(name, sell_in, quality) }
 
   describe "#update_quality" do
-    context "Sulfuras" do
-      it "does" do
-      
+    context "any item" do
+      let!(:name) { "Ether" }
+      it "increases quality of other items" do
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 4
+      end
+
+      it "decreases quality of other items" do
+        item.sell_in = -1
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 3
       end
     end
 
     context "Aged Brie" do
-      it "does" do
-      
+      let!(:name) { "Aged Brie" }
+      it "checks if Aged Brie increases quality" do
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 6
+      end
+
+      it "checks if Aged Brie increases quality twice" do
+        item.sell_in = -1
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 7
       end
     end
 
-  end
+    context "Sulfuras" do
+      let!(:name) { "Sulfuras, Hand of Ragnaros" }
+      it "checks if Sulfuras quality doesn't change" do
+        GildedRose.new([item]).update_quality
 
+        expect(item.quality).to eq 5
+      end
+      it "checks if Sulfuras sell_in decreases by 1" do
+        GildedRose.new([item]).update_quality
+
+        expect(item.sell_in).to eq 5
+      end
+    end
+
+    context "Conjured" do
+      let!(:name) { "Conjured item" }
+      it "checks if conjured quality decreases by 2" do
+        GildedRose.new([item]).update_quality
+
+        expect(item.sell_in).to eq 4
+        expect(item.quality).to eq 3
+      end
+
+      it "If sell_in has pass, quality degrades by 4" do
+        item.sell_in = 0
+        GildedRose.new([item]).update_quality
+
+        expect(item.sell_in).to eq 0
+        expect(item.quality).to eq 1
+      end
+    end
+
+    context "Backstage passes" do
+      let!(:name) { "Backstage passes to a TAFKAL80ETC concert" }
+      it "checks if it increases by three when sell in 5 days or less" do
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 8
+      end
+
+
+      it "checks if it increases by two when sell in 10 days or less" do
+        item.sell_in= 10
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 7
+      end
+
+      it "checks quality is zero when concert passed" do
+        item.sell_in= 0
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 0
+
+      end
+    end
+
+    context "Quality validation" do
+      let!(:name) { "Ether" }
+      it "checks if it doesn't return a negative value" do
+        item.quality = 0
+
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 0
+      end
+
+     it "checks if it doesn't exceed 50" do
+        item.name = "Aged Brie"
+        item.quality = 50
+
+        GildedRose.new([item]).update_quality
+
+        expect(item.quality).to eq 50
+      end
+    end
+  end
 end
